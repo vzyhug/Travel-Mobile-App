e6ccf0d1a31ac78570c2a71b0d6e0b3c1c80e73a
VietLong <longdinh231005@gmail.com>
2026-05-15 16:01:38 +0700

    Explore (UI5) va DetailExplore (UI6)

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
-    return MaterialApp(debugShowCheckedModeBanner: false, home: ExploreScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
   }
 }
