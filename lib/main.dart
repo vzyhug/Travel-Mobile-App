ecef117d93867ff7a8aff75780fc2f10c2db6940
VietLong <longdinh231005@gmail.com>
2026-05-15 13:20:48 +0700

    Hoan thanh giao dien Explore (UI5) va DetailExplore (UI6)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_application/screens/login_screen.dart';
-
+import 'package:travel_application/screens/explore_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    //return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ExploreScreen());
   }
 }
